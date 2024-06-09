1fd7f798232c3f4c509022f3b06f8c286b6325f1
Oussama_Achour <oachour08@gmail.com>
2024-06-09 18:44:13 +0100

    Reformatting previous commits - Backend; Add : function to format file name before storing in blob Fix : data formatting befor updating rows in DB

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,20 @@
 from flask import Flask, Blueprint, request, jsonify,send_from_directory
 from flask_cors import CORS, cross_origin
 
-from model import predict
-from tensorflow.keras.models import load_model
+# from tensorflow.keras.models import load_model
 import pandas
 from werkzeug.utils import secure_filename
 
 import os
 from dotenv import load_dotenv
+
+import numpy 
+from PIL import Image
+
+from model import predict , segment, load_classification_model
+import threading
+
+
 #Blob
 from azure.storage.blob import BlobServiceClient
 
@@ -17,10 +24,14 @@ from azure import identity
 from typing import Union
 # from fastapi import FastAPI
 from pydantic import BaseModel
+from pydantic.json import pydantic_encoder
+
 
 #for connecting with service principal
 import msal
 import adal
+
+import json
 
 
 
@@ -31,23 +42,44 @@ load_dotenv()
 app = Flask(__name__)
 cors = CORS(app)
 UPLOAD_FOLDER = './input_images'
+SEGMENTED_IMAGES='./segmented-images'
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SEGMENTED_IMAGES'] = SEGMENTED_IMAGES
 
 
 #Connection strings to SQL DB and Blob Storage
 sql_connection_string = os.environ["AZURE_SQL_CONNECTION_STRING"]
 blob_connection_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+container_name = os.environ["CONTAINER_NAME"]
 
 
+model=load_classification_model()
+
+
+class PredictedProbabilities(BaseModel):
+    bad: float
+    average: float
+    good: float
+    def __init__(self, bad: float, average: float, good:float, decimal_places: int = 2):
+        super().__init__(bad=round(bad, decimal_places),
+                        average=round(average, decimal_places),
+                        good=round(good, decimal_places)
+                        )
+
+   
+class ResultModel(BaseModel):
+    predicted_class: str
+    predicted_probabilities: PredictedProbabilities
 
 class Embryo(BaseModel):
 
     # ImageUrl: Union[str, None] = None
     ImageName:str
-    Result: str
+    Result:ResultModel
     suggested_value:Union[str, None] = None
     note:Union[str, None] = None
+
 
 
 @app.route('/')
@@ -72,49 +104,89 @@ def upload_image():
             #saving image locally
             uploaded_file.save(destination_file)
 
-            #saving image in blob
-            saveToBlob(uploaded_file,destination_file)
+            # #saving image in blob
+            # saveToBlob(uploaded_file,destination_file)
 
-            #processing the image
-            result = process_image(filename)
+            #processing the image : class prediction & segmentation
+            predicted_probabilities={}
+            predicted_class,predicted_probabilities=process_image(filename,destination_file)
+            # print("predicted_class{}\npredicted_probabilities{}".format(predicted_class,predicted_probabilities))
+            
+            # loading the json object to extract data
+            predicted_probabilities=json.loads(predicted_probabilities)
 
+            predicted_probabilities=PredictedProbabilities(bad=predicted_probabilities['bad'],average=predicted_probabilities['average'],good=predicted_probabilities['good'])
+            result=ResultModel(predicted_class=predicted_class,predicted_probabilities=predicted_probabilities)            
+            
             #saving image in sql db
             embryo= Embryo(ImageName=filename,Result=result,suggested_value=None,note=None)
-            saveToSQLDB(embryo)
-
-            #Retrieve image and result
-            # print(get_image_and_result(filename))
+            
+            #converting final result to json string 
+            result=json.dumps(result,default=pydantic_encoder)
+            print("final result",result)
 
             # Embed the result with the image URL
-            image_with_result = {'image':destination_file , 'result': result}
-            # This are returns for just the upload
-            #     return jsonify({'message': 'File uploaded successfully', 'file': filename}), 201
-            # else:
-            #     return jsonify({'error': 'Invalid file type, only JPEG images are allowed'}), 400
+            image_with_result = {'image_name':filename , 'result': result}
+
+            # Saving image in Blob and image results in SQL DB : These task run in background 
+            print("thread begins")
+            threading.Thread(target=saveToBlob, args=(uploaded_file, destination_file)).start()
+            threading.Thread(target=saveToSQLDB, args=(embryo,predicted_probabilities)).start()
+
+        
 
             return jsonify({'message': 'Image processed successfully', 'image_with_result': image_with_result}), 201
+            # return jsonify({'message': 'Image processed successfully'}), 201
+
         else:
             return jsonify({'error': 'Invalid file type, only JPEG images are allowed'}), 400
-
-
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'jpg','png', 'jpeg','bmp'}
 
-def process_image(image_name):
+# def load_model():
 
-    predict()
+
+def process_image(image_name,image_path):
+    
+    #image segmentation
+    segmented_image=segment(image_path)
+    segmented_image_path = os.path.join(app.config['SEGMENTED_IMAGES'], image_name)
+    #saving image locally
+    # segmented_image.save(segmented_image_path)
+    # segmented_image = segmented_image.astype(np.uint8)  # Convert the data to uint8 type
+
+    # Convert the NumPy array to a Pillow image
+    image_pil = Image.fromarray(segmented_image.astype(numpy.uint8))
+
+    # Save the image
+    image_pil.save(segmented_image_path)
+
+
+    #class prediction
+    percentages={}
+    print("begin predict")
+
+    predicted_probabilities=predict(model=model,image_path=image_path,image_name=image_name)
+    print("end predict")
+
     # Load the results CSV file
-    labled_results_csv_file='./results/labled_results_csv.csv'
-    results_df = pandas.read_csv(labled_results_csv_file)
+    labeled_results_csv_file='./results/labeled_results_csv.csv'
+    results_df = pandas.read_csv(labeled_results_csv_file)
 
     row = results_df.loc[results_df['image_name'] == image_name]
-    result=row['classification'].to_string(index=False)
-    print(image_name)
-    print(result)
-    return result
+    print("row",row)
+    predicted_class=row['classification'].to_string(index=False)
+    # print(image_name)
+    # print(result)
+    print("predicted_class{}\npredicted_probabilities{}".format(predicted_class,predicted_probabilities))
+
+    return predicted_class , predicted_probabilities
 
 
 # Function to establish connection to blob storage
@@ -123,7 +195,6 @@ def get_blob_service_client():
     return BlobServiceClient.from_connection_string(blob_connection_str)
 
 # retrieve the container name (in which images will be store in the storage account)  from the environment variable
-container_name = os.environ["CONTAINER_NAME"]
 def saveToBlob(file,filepath):
     blob_service_client=get_blob_service_client()
     try:
@@ -137,27 +208,29 @@ def saveToBlob(file,filepath):
 
     try:
         with open(file=filepath, mode="rb") as data:
-            print("before blob")
-            blob_client = container_client.upload_blob(name=file.filename, data=data, overwrite=True)
+            #deleting [ , ] symbols from file name 
+            
+            blob_client = container_client.upload_blob(name=remove_square_brackets(file.filename), data=data, overwrite=True)
             print("after blob")
     except Exception as e:
         print(e)
         print("Ignoring duplicate filenames") # ignore duplicate filenames
 
+def remove_square_brackets(filename):
+    # Replace square brackets with an empty string
+    return filename.replace('[', '').replace(']', '')
 
-def saveToSQLDB(item: Embryo):
+def saveToSQLDB(item: Embryo,predicted_probabilities:PredictedProbabilities):
     print("********* saving data to DB .... ********* ")
     #Environments variables to have access to Blob Storage
     sas_token=os.environ["SAS_TOKEN"]
     storage_account_url=os.environ["STORAGE_ACCOUNT_URL"]
 
     try:
-        
+    
         # conn = get_conn()
         conn=connect_oauth()
         cursor = conn.cursor()
-
-        #Apparently this is why it doesn't recognize the storage account , the string being overwritten and null
 
         # Table should be created ahead of time in production app.
         cursor.execute("""
@@ -165,7 +238,8 @@ def saveToSQLDB(item: Embryo):
             CREATE TABLE EmbryoResults (
                 Id INT NOT NULL PRIMARY KEY IDENTITY,
                 ImageName NVARCHAR(MAX),
-                Result NVARCHAR(MAX),
+                PredictedClass NVARCHAR(MAX),
+                PredictedProbabilities NVARCHAR(MAX),
                 SuggestedValue NVARCHAR(MAX) NULL,
                 Note NVARCHAR(MAX) NULL
             );
@@ -173,55 +247,18 @@ def saveToSQLDB(item: Embryo):
 
         conn.commit()
 
-        # This is two sql queries are for retrieving image url and storing it in DB
-
-        # #creating external Azure Blob data source
-        # cursor.execute("""
-        #     CREATE DATABASE SCOPED CREDENTIAL sampleblobcred1
-        #         WITH IDENTITY = 'SHARED ACCESS SIGNATURE',
-        #         SECRET = ?;
-        # """,(sas_token,))
-
-        # conn.commit()
-
-
-        # cursor.execute("""
-        #     CREATE EXTERNAL DATA SOURCE blobstorage
-        #     WITH (
-        #         TYPE = BLOB_STORAGE,
-        #         LOCATION = ?,
-        #         CREDENTIAL = sampleblobcred1);
-        #     """, (storage_account_url,))
-
-
-
         # conn.commit()
     except Exception as e:
         # Table may already exist
         print(e)
 
     print("******* Inserting new row ... *********")
-    cursor.execute(f"INSERT INTO EmbryoResults (ImageName, Result,SuggestedValue,Note) VALUES (?,?,?,?)", item.ImageName, item.Result,item.suggested_value,item.note)
-
-    # Insert image url into table from blob storage using parameterized query
-
-    # bulk="pictures/{}".format(item.ImageName)
-    # print("bulk",bulk)
-    # cursor.execute(f"INSERT INTO EmbryoResults (ImageUrl, ImageName, Result) VALUES ((SELECT BulkColumn FROM OPENROWSET(BULK ?,DATA_SOURCE = 'bolobstorage', SINGLE_BLOB) AS ImageFile), ?,?)", bulk, item.ImageName , item.Result)
-    # sql = """
-    # INSERT INTO EmbryoResults (ImageUrl)
-    # VALUES (
-    #     (SELECT * FROM OPENROWSET(
-    #         BULK 'pictures/personal.jpeg',
-    #         DATA_SOURCE = 'blobstorage',
-    #         SINGLE_BLOB
-    #         ) AS ImageFile)
-
-    # )
-    # """
-    # cursor.execute(sql)
+    cursor.execute(f"INSERT INTO EmbryoResults (ImageName, PredictedClass,PredictedProbabilities,SuggestedValue,Note) VALUES (?,?,?,?,?)", item.ImageName, item.Result.predicted_class,json.dumps(predicted_probabilities,default=pydantic_encoder),item.suggested_value,item.note)
+    # cursor.execute(f"INSERT INTO EmbryoResults (ImageName, PredictedClass,PredictedProbabilities,SuggestedValue,Note) VALUES (?,?,?,?,?)", item.ImageName, "good","hello",item.suggested_value,item.note)
 
     conn.commit()
+
+    print("successfully added in sql db")
     return item
 
 ### connected with sql db in default way
@@ -274,19 +311,22 @@ def get_all_results():
 
     conn = get_conn()
     cursor = conn.cursor()
-    cursor.execute("SELECT ImageName, Result, SuggestedValue, Note FROM EmbryoResults")
+    cursor.execute("SELECT ImageName, PredictedClass, PredictedProbabilities, SuggestedValue, Note FROM EmbryoResults")
     rows = cursor.fetchall()
     print("rows",rows)
     conn.close()
     # return {row.ImageName: row.Result for row in results}
     results = {}
 
+
     for row in rows:
         results[row.ImageName] = {
-            'result': row.Result,
+            'predicted_class': row.PredictedClass,
+            'predicted_probabilities': row.PredictedProbabilities,
             'suggested_value': row.SuggestedValue,
             'note': row.Note
         }
+    
     print("results",results)
 
     return results
@@ -302,25 +342,21 @@ def list_images_and_results():
 
         # Combine the data
         response = []
-        # for image in images:
-        #     # destination_file = os.path.join(app.config['UPLOAD_FOLDER'], image)
 
-        #     response.append({
-        #         "image_name":image,
-        #         "image_path": f"http://localhost:5000/images/{image}",
-        #         "result": results.get(image, "No result found")
-        #     })
-        print("results",results)
         for image in images:
             result_data = results.get(image, {
-                'result': "No result found",
+                'predicted_class': "No result found",
+                'predicted_probabilities': "No result found",
                 'suggested_value': None,
                 'note': None
                 })
+            # image_name=result_data['image_name']
             response.append({
                 "image_name": image,
                 "image_path": f"http://localhost:5000/images/{image}",
-                "result": result_data['result'],
+                "segmented_image_path":f"http://localhost:5000/segmented-images/{image}",
+                "predicted_class": result_data['predicted_class'],
+                "predicted_probabilities": result_data['predicted_probabilities'],
                 "suggested_value": result_data['suggested_value'],
                 "note": result_data['note']
             })
@@ -333,13 +369,18 @@ def list_images_and_results():
 def get_image(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
+@app.route('/segmented-images/<path:filename>')
+def get_segmented_image(filename):
+    return send_from_directory(SEGMENTED_IMAGES, filename)
+
 @app.route('/update', methods=['POST'])
 def update_embryo():
     data = request.json
     print(data)
     image_name=data['image_name']
+    predicted_class=data['predicted_class']
+    predicted_probabilities=data['predicted_probabilities']
     suggested_value = data['suggested_value']
-    result=data['result']
     note = data['note']
     try:
         conn = connect_oauth()
@@ -355,10 +396,11 @@ def update_embryo():
     # Update the embryo with new values
         cursor.execute("""
             UPDATE EmbryoResults
-            SET Result = ?, SuggestedValue = ?, Note = ?
-            WHERE ImageName = ?""", (result, suggested_value, note, image_name)
+            SET PredictedClass = ?, PredictedProbabilities = ?, SuggestedValue = ?, Note = ?
+            WHERE ImageName = ?""", (predicted_class, predicted_probabilities, suggested_value, note, image_name)
              
             )
+
 
         conn.commit()
         conn.close()
@@ -371,23 +413,7 @@ def update_embryo():
             conn.close()
         return jsonify({'error': str(e)}), 500
 
-
-
-
-    # # Retrieve the embryo from the database based on the image name
-    # embryo = Embryo.query.filter_by(image_name=data['image_name']).first()
-    # if not embryo:
-    #     return jsonify({'error': 'Embryo not found'}), 404
-
-    # Update the note and suggested value
-
-    # # Save the changes to the database
-    # db.session.commit()
-
-    # update_embryo=Embryo(ImageName=image_name,Result=result,SuggestedValue=suggested_value,Note=note)
-    # saveToSQLDB(update_embryo)
-
-    # return jsonify({'message': 'Embryo updated successfully'}), 200    
+  
 
 
 ###  THIS DOESN'T WORK !! :   API endpoint to retrieve image and result
--- backend/model.py
+++ backend/model.py
@@ -1,91 +1,200 @@
+import tensorflow as tf
 from tensorflow.keras.models import load_model
 import numpy as np
 import os
 from tqdm import tqdm
 import cv2
 from albumentations import CLAHE
+import json
+from pydantic.json import pydantic_encoder
+from PIL import Image
+import matplotlib.pyplot as plt
+
 
 import pandas 
 
-def predict():
-    # Folder containing test images
-    test_image_folder = './input_images/'
 
-    # Get a list of file names in the test image folder
-    test_image_names = os.listdir(test_image_folder)
-
-    # Preprocess test images
-    test_images = []
-    for image_name in tqdm(test_image_names):
-        # Concatenate image folder path with file name
-        image_path = os.path.join(test_image_folder, image_name)
-
-        # Check if the file is an image
-        if not image_name.endswith(('.BMP', '.jpeg', '.png', '.bmp', '.gif')):
-            print(f"Skipping non-image file: {image_name}")
-            continue
-
-        # Read the image
-        image = cv2.imread(image_path)
-
-        # Check if the image was read successfully
-        if image is None:
-            print(f"Error reading image: {image_path}")
-            continue
-
-        # Apply CLAHE to each channel of the image
-        clahe = CLAHE(clip_limit=2.0, tile_grid_size=(1, 1))
-        enhanced_image = clahe(image=image)['image']
-
-        # Resize the image to the same size as training images (299x299)
-        resized_image = cv2.resize(enhanced_image, (299, 299))
-
-        # Normalize the pixel values to the range [0, 1]
-        normalized_image = resized_image.astype(np.float32) / 255.0
-
-        # Add the preprocessed image to the list
-        test_images.append(normalized_image)
-
-    # Convert the list of images to a numpy array
-    test_images = np.array(test_images)
-
-
+def load_classification_model():
+    print("################ Loading Classification Model ################")
     #loading the model
     MODEL_PATH="./models/Images_checkpoint.h5"
     # INPUT_PATH="./input_images/MTL-0130-1652-5E96-P09-FP4.jpg_0_0.bmp"
     model = load_model(MODEL_PATH)
     model.summary()
-    predictions=model.predict(test_images)
-    print("******** predictions: ",predictions)
+    return model
 
+def predict(model,image_path,image_name):
+    # Folder containing test images
+    test_image_folder = './input_images/'
+
+    # Get a list of file names in the test image folder
+    # test_image_names = os.listdir(test_image_folder)
+
+    # Preprocess test images
+    images = []
+    
+    if not image_path.endswith(('.BMP', '.jpeg', '.png', '.bmp', '.jpg', '.gif')):
+        print(f"File extension not compatible: {image_path}")
+        raise Exception("File extension not compatible")
+
+    # Read the image
+    image = cv2.imread(image_path)
+
+    # Check if the image was read successfully
+    if image is None:
+        print(f"Error reading image: {image_path}")
+        raise Exception("Error reading image")
+
+
+    # Apply CLAHE to each channel of the image
+    clahe = CLAHE(clip_limit=2.0, tile_grid_size=(1, 1))
+    enhanced_image = clahe(image=image)['image']
+
+    # Resize the image to the same size as training images (299x299)
+    resized_image = cv2.resize(enhanced_image, (299, 299))
+
+    # Normalize the pixel values to the range [0, 1]
+    normalized_image = resized_image.astype(np.float32) / 255.0
+
+    # Add the preprocessed image to the list
+    images.append(normalized_image)
+
+    # Convert the list of images to a numpy array
+    images = np.array(images)
+
+
+    # #loading the model
+    # MODEL_PATH="./models/Images_checkpoint.h5"
+    # # INPUT_PATH="./input_images/MTL-0130-1652-5E96-P09-FP4.jpg_0_0.bmp"
+    # model = load_model(MODEL_PATH)
+    # model.summary()
+
+    print("before predict from predict")
+    predictions=model.predict(images)
+    print(predictions)
+    print("after predict from predict")
+
+    #pourcentages 
+    pourcentages={}
+    pourcentages={
+        "bad":predictions[0][0]*100,
+        "average":predictions[0][2]*100,
+        "good":predictions[0][1]*100
+
+    }
     # Get predicted classes
     predicted_classes = np.argmax(predictions, axis=1)
 
+    pourcentages = json.dumps(pourcentages,default=pydantic_encoder)
 
-    # Create a DataFrame with image names and predictions
-    results_df = pandas.DataFrame({'image_name': test_image_names, 'classification': predicted_classes})
-
-    # Save the DataFrame to a CSV file
-    results_csv_file = './results/results_csv.csv'
-    results_df.to_csv(results_csv_file, index=False)
-    print("Predictions saved to:", results_csv_file)
+    # print("******** pourcentages: ",pourcentages)
 
 
-    # Load the results CSV file
-    results_df = pandas.read_csv(results_csv_file)
-
-    # Replace numeric classifications with labels
-    classification_labels = {1: 'good', 2: 'average', 3: 'bad'}
-    # print(results_df['classification'])
-    # print(results_df['classification'].to_string(index=False))
     
-    results_df['classification'] = results_df['classification'].map(classification_labels)
+    # # Save the DataFrame to a CSV file
+    results_csv_file = './results/results_csv.csv'
+    results_df = pandas.DataFrame({'image_name': image_name, 'classification': predicted_classes})
+
+    if os.path.isfile(results_csv_file):
+        # Read the existing CSV file
+        existing_df = pandas.read_csv(results_csv_file)
+        
+        # Check if the image name already exists
+        if image_name in existing_df['image_name'].values:
+            print(f"Image name {image_name} already exists in the CSV file. Skipping append.")
+        else:
+            # Append the new DataFrame to the existing CSV file
+            results_df.to_csv(results_csv_file, mode='a', header=False, index=False)
+            print(f"Appended {image_name} to the CSV file.")
+    else:
+        # If the file does not exist, write the DataFrame with the header
+        results_df.to_csv(results_csv_file, index=False)
+        print(f"Created new CSV file and added {image_name}.")
+
 
     # Save the updated DataFrame to a new CSV file
-    labled_results_csv_file = './results/labled_results_csv.csv'
-    results_df.to_csv(labled_results_csv_file, index=False)
-    print("Labled predictions saved to:", labled_results_csv_file)
+    # Replace numeric classifications with labels
+    classification_labels = {1: 'good', 2: 'average', 3: 'bad'}
+    results_df['classification'] = results_df['classification'].map(classification_labels)
+
+    labeled_results_csv_file = './results/labeled_results_csv.csv'
+    if os.path.isfile(labeled_results_csv_file):
+        # Read the existing CSV file
+        existing_labeled_df = pandas.read_csv(labeled_results_csv_file)
+        
+        # Check if the image name already exists
+        if image_name in existing_labeled_df['image_name'].values:
+            print(f"Image name {image_name} already exists in the labeled CSV file. Skipping append.")
+        else:
+            # Append the new DataFrame to the existing CSV file
+            # results_df = pandas.DataFrame({'image_name': image_name, 'classification': predicted_classes})
+            results_df.to_csv(labeled_results_csv_file, mode='a', header=False, index=False)
+            print(f"Appended {image_name} to the labeled CSV file.")
+    else:
+        # If the file does not exist, write the DataFrame with the header
+        results_df.to_csv(results_csv_file, index=False)
+        print(f"Created new labeled CSV file and added {image_name}.")
+    # results_df.to_csv(labled_results_csv_file,mode='a', header=False, index=False)
+    # print("Labled predictions saved to:", labled_results_csv_file)
+    return pourcentages
+
+
+# predict()
+
+def segment(image_path):
+    ## This decorators ensures that Keras knows how to serialize and deserialize the function when saving and loading the model.
+    # @tf.keras.utils.register_keras_serializable()
+    def iou(y_true, y_pred):
+        def f(y_true, y_pred):
+            intersection = (y_true * y_pred).sum()
+            union = y_true.sum() + y_pred.sum() - intersection
+            x = (intersection + 1e-15) / (union + 1e-15)
+            x = x.astype(np.float32)
+            return x
+        return tf.numpy_function(f, [y_true, y_pred], tf.float32)
+
+    smooth = 1e-15
+
+    @tf.keras.utils.register_keras_serializable()
+    def dice_coef(y_true, y_pred):
+        y_true = tf.keras.layers.Flatten()(y_true)
+        y_pred = tf.keras.layers.Flatten()(y_pred)
+        intersection = tf.reduce_sum(y_true * y_pred)
+        return (2. * intersection + smooth) / (tf.reduce_sum(y_true) + tf.reduce_sum(y_pred) + smooth)
+
+    def dice_loss(y_true, y_pred):
+        return 1.0 - dice_coef(y_true, y_pred)
+
+    ##loading the model 
+    SEGMENTATION_MODEL="./models/model_unet.h5"
+    model = load_model(SEGMENTATION_MODEL, custom_objects={'iou': iou, 'dice_coef': dice_coef})
+
+    ##segment image
+    image = Image.open(image_path)
+    plt.imshow(image)
+    # plt.show()
+    # Resize the image to match the expected input shape of the model
+    image = np.array(image)
+
+    x = cv2.resize(image, (256, 256))
+
+    # Normalize the image
+    # x=image
+    x = x / 255.0
+    x = x.astype(np.float32)
+
+    # Perform prediction
+    p = model.predict(np.expand_dims(x, axis=0))[0]
+    print("segmentation successful")
+    # Convert prediction to mask
+    p = np.argmax(p, axis=-1)
+    p = np.expand_dims(p, axis=-1)
+    p = p * (255 / 6)
+    p = p.astype(np.int32)
+    p = np.concatenate([p, p, p], axis=2)
+
+    print("image successfully segmented")
+    return p
 
 
 
-predict()
