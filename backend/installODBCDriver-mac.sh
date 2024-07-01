2035067f0216e75945b48a27e191eadbfb4f7a69
MacBook <macbook@MacBook-Pro.local>
2024-07-01 11:52:45 +0100

    Add : ODBCDriver installation script for macOS

---
+++ backend/installODBCDriver-mac.sh
@@ -0,0 +1,4 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew tap microsoft/mssql-release https://github.com/Microsoft/homebrew-mssql-release
+brew update
+HOMEBREW_ACCEPT_EULA=Y brew install msodbcsql18 mssql-tools18
